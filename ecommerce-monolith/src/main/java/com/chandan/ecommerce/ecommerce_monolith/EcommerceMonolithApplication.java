ccfbfe1876083a74d4241e0b14e90e0d28e08985
Chandan Shukla <chandanshukla759@gmail.com>
2025-12-04 23:24:06 +0530

    intial commit project skeleton

---
+++ ecommerce-monolith/src/main/java/com/chandan/ecommerce/ecommerce_monolith/EcommerceMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.chandan.ecommerce.ecommerce_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMonolithApplication.class, args);
+	}
+
+}
