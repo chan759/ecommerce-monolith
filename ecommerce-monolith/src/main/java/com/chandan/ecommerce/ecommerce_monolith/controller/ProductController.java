7769cd62f537615b7e7781d64a31616f3edd1eb7
Chandan Shukla <chandanshukla759@gmail.com>
2025-12-05 00:09:31 +0530

    product controller added

---
+++ ecommerce-monolith/src/main/java/com/chandan/ecommerce/ecommerce_monolith/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.chandan.ecommerce.ecommerce_monolith.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping
+    public List<String> getAllProducts(){
+        return List.of("Phone","Laptop","Headset");
+    }
+}
